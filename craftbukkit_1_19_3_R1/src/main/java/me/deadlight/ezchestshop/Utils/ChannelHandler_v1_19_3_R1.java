0c5290a19b0f5225d7af169e9698a34696321603
Amirreza <20891968+ItzAmirreza@users.noreply.github.com>
2023-03-04 16:33:38 +0100

    tst

---
+++ craftbukkit_1_19_3_R1/src/main/java/me/deadlight/ezchestshop/Utils/ChannelHandler_v1_19_3_R1.java
@@ -0,0 +1,49 @@
+package me.deadlight.ezchestshop.Utils;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import net.minecraft.network.protocol.game.PacketPlayInUpdateSign;
+import org.bukkit.entity.Player;
+import java.lang.reflect.Field;
+import java.util.Map;
+
+public class ChannelHandler_v1_19_R1 extends ChannelInboundHandlerAdapter {
+
+    private final Player player;
+    private static Field updateSignArrays;
+
+    static {
+        try {
+            updateSignArrays = PacketPlayInUpdateSign.class.getDeclaredField("c");
+            updateSignArrays.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ChannelHandler_v1_19_R1(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+
+        if (msg instanceof PacketPlayInUpdateSign) {
+            for (Map.Entry<SignMenuFactory, UpdateSignListener> entry : v1_19_R1.getListeners().entrySet()) {
+                UpdateSignListener listener = entry.getValue();
+
+                try {
+                    listener.listen(player, (String[]) updateSignArrays.get(msg));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+
+                if (listener.isCancelled()) {
+                    return;
+                }
+            }
+        }
+
+        ctx.fireChannelRead(msg);
+    }
+
+}
---
+++ craftbukkit_1_19_3_R1/src/main/java/me/deadlight/ezchestshop/Utils/v1_19_3_R1.java
@@ -0,0 +1,180 @@
+package me.deadlight.ezchestshop.Utils;
+
+import io.netty.channel.Channel;
+import me.deadlight.ezchestshop.EzChestShop;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.chat.IChatBaseComponent;
+import net.minecraft.network.protocol.game.PacketPlayOutEntityMetadata;
+import net.minecraft.network.protocol.game.PacketPlayOutSpawnEntity;
+import net.minecraft.server.level.EntityPlayer;
+import net.minecraft.server.network.PlayerConnection;
+import net.minecraft.world.entity.decoration.EntityArmorStand;
+import net.minecraft.world.entity.item.EntityItem;
+import net.minecraft.world.level.World;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_19_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_19_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class v1_19_R1 extends VersionUtils {
+
+    private static final Map<SignMenuFactory, UpdateSignListener> listeners = new HashMap<>();
+
+    /**
+     * Convert a Item to a Text Compount. Used in Text Component Builders to show
+     * items in chat.
+     *
+     * @category ItemUtils
+     * @param itemStack
+     * @return
+     */
+
+    @Override
+    String ItemToTextCompoundString(ItemStack itemStack) {
+        // First we convert the item stack into an NMS itemstack
+        net.minecraft.world.item.ItemStack nmsItemStack = CraftItemStack.asNMSCopy(itemStack);
+        NBTTagCompound compound = new NBTTagCompound();
+        compound = nmsItemStack.b(compound);
+
+        return compound.toString();
+    }
+
+    @Override
+    int getArmorStandIndex() {
+        return 15;
+    }
+
+    @Override
+    int getItemIndex() {
+        return 8;
+    }
+
+    @Override
+    void destroyEntity(Player player, int entityID) {
+        ((org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer) player).getHandle().b.a(new net.minecraft.network.protocol.game.PacketPlayOutEntityDestroy(entityID));
+    }
+
+    @Override
+    void spawnHologram(Player player, Location location, String line, int ID) {
+
+        CraftPlayer craftPlayer = (CraftPlayer) player;
+        EntityPlayer entityPlayer = craftPlayer.getHandle();
+        PlayerConnection playerConnection = entityPlayer.b;
+        CraftWorld craftWorld = (CraftWorld) location.getWorld();
+        World world = craftWorld.getHandle();
+        //------------------------------------------------------
+
+        EntityArmorStand armorstand = new EntityArmorStand(world, location.getX(), location.getY(), location.getZ());
+        armorstand.j(true); //invisible
+        armorstand.t(true); //Marker
+        armorstand.b(IChatBaseComponent.a(line)); //set custom name
+        armorstand.n(true); //make custom name visible
+        armorstand.e(true); //no gravity
+        armorstand.e(ID); //set entity id
+
+        PacketPlayOutSpawnEntity packetPlayOutSpawnEntity = new PacketPlayOutSpawnEntity(armorstand, 0);
+        playerConnection.a(packetPlayOutSpawnEntity);
+        //------------------------------------------------------
+        // sending meta packet
+        PacketPlayOutEntityMetadata metaPacket = new PacketPlayOutEntityMetadata(ID, armorstand.ai(), true);
+        playerConnection.a(metaPacket);
+
+    }
+
+    @Override
+    void spawnFloatingItem(Player player, Location location, ItemStack itemStack, int ID) {
+
+        CraftPlayer craftPlayer = (CraftPlayer) player;
+        EntityPlayer entityPlayer = craftPlayer.getHandle();
+        PlayerConnection playerConnection = entityPlayer.b;
+        CraftWorld craftWorld = (CraftWorld) location.getWorld();
+        World world = craftWorld.getHandle();
+        //------------------------------------------------------
+
+        EntityItem floatingItem = new EntityItem(world, location.getX(), location.getY(), location.getZ(), CraftItemStack.asNMSCopy(itemStack));
+        floatingItem.e(true); //no gravity
+        floatingItem.e(ID); //set entity id
+        floatingItem.n(0, 0, 0); //set velocity
+
+        PacketPlayOutSpawnEntity packetPlayOutSpawnEntity = new PacketPlayOutSpawnEntity(floatingItem, 0);
+        playerConnection.a(packetPlayOutSpawnEntity);
+        //------------------------------------------------------
+        // sending meta packet
+        PacketPlayOutEntityMetadata metaPacket = new PacketPlayOutEntityMetadata(ID, floatingItem.ai(), true);
+        playerConnection.a(metaPacket);
+
+    }
+
+    @Override
+    void signFactoryListen(SignMenuFactory signMenuFactory) {
+
+        listeners.put(signMenuFactory, new UpdateSignListener() {
+            @Override
+            public void listen(Player player, String[] array) {
+
+                SignMenuFactory.Menu menu = signMenuFactory.getInputs().remove(player);
+
+                if (menu == null) {
+                    return;
+                }
+                setCancelled(true);
+
+                boolean success = menu.getResponse().test(player, array);
+
+                if (!success && menu.isReopenIfFail() && !menu.isForceClose()) {
+                    Bukkit.getScheduler().runTaskLater(EzChestShop.getPlugin(), () -> menu.open(player), 2L);
+                }
+
+                if (success) {
+                    removeSignMenuFactoryListen(signMenuFactory);
+                }
+
+                Bukkit.getScheduler().runTaskLater(EzChestShop.getPlugin(), () -> {
+                    if (player.isOnline()) {
+                        Location location = menu.getLocation();
+                        player.sendBlockChange(location, location.getBlock().getBlockData());
+                    }
+                }, 2L);
+
+
+            }
+        });
+
+    }
+
+    @Override
+    void removeSignMenuFactoryListen(SignMenuFactory signMenuFactory) {
+
+        listeners.remove(signMenuFactory);
+
+    }
+
+    @Override
+    void openMenu(SignMenuFactory.Menu menu, Player player) {
+        MenuOpener_v1_19_R1.openMenu(menu, player);
+    }
+
+    @Override
+    public void injectConnection(Player player) {
+        ((CraftPlayer) player).getHandle().b.a().m.pipeline().addBefore("packet_handler", "ecs_listener", new ChannelHandler_v1_19_R1(player));
+    }
+
+    @Override
+    public void ejectConnection(Player player) {
+
+        Channel channel = ((CraftPlayer) player).getHandle().b.a().m;
+        channel.eventLoop().submit(() -> channel.pipeline().remove("ecs_listener"));
+    }
+
+    public static Map<SignMenuFactory, UpdateSignListener> getListeners() {
+        return listeners;
+    }
+
+
+}
