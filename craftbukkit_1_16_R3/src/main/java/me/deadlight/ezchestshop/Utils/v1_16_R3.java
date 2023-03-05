1d89ba12615219c952e89c7b9ee0a7747636512d
Amirreza <20891968+ItzAmirreza@users.noreply.github.com>
2023-03-05 14:21:53 +0100

    fix floating item flying away 1.1.6.5

--- craftbukkit_1_16_R3/src/main/java/me/deadlight/ezchestshop/Utils/v1_16_R3.java
+++ craftbukkit_1_16_R3/src/main/java/me/deadlight/ezchestshop/Utils/v1_16_R3.java
@@ -99,6 +99,11 @@ public class v1_16_R3 extends VersionUtils {
         PacketPlayOutEntityMetadata metaPacket = new PacketPlayOutEntityMetadata(ID, floatingItem.getDataWatcher(), true);
         playerConnection.sendPacket(metaPacket);
 
+        //sending a velocity packet
+        floatingItem.setMot(0, 0, 0);
+        PacketPlayOutEntityVelocity velocityPacket = new PacketPlayOutEntityVelocity(floatingItem);
+        playerConnection.sendPacket(velocityPacket);
+
     }
 
     @Override
